5d1a817250ef9530f0f23d1cf0d52f693e560273
AlvaroQP <alonsol1569@gmail.com>
2023-10-28 19:26:38 +0200

    28-10-2023

--- Actividades/kraprekar/script.js
+++ Actividades/kraprekar/script.js
@@ -1,44 +1,72 @@
 let numeroUsuario = prompt("Dime un numero de cuatro cifras");
-let arrayNumeroUsuario = new Array(4);
-let contadorKraprekar = 0;
-//Array donde se guardaran el numero de forma ascendente y descendente
-let numeroUsuarioAscendente = new Array(4);
-let numeroUsuarioDescendente = new Array(4);
 
-//
-function convertirNumeroUsuario(){
-    //Bucle que llena el arrayNumeroUsuario con el numero del usuario y los transforma a un numero cada elemento 
-    for(let i = 0; i < 4; i++){
-        arrayNumeroUsuario[i] = +numeroUsuario.charAt(i);
-        console.log(arrayNumeroUsuario);
+let llegoAKraprekar = false;
+
+//Array donde se guardaran el numero de forma ascendente y descendente
+let arrayAscendente = new Array(4);
+let arrayDescendente = new Array(4);
+
+function verificarNumero(numero){
+    
+}
+
+//Convierte el numero en un string y kuego los introduce a un array ya con sus elementos transformados a numero
+function convertirNumero(numero){
+
+    let numeroAString = numero.toString();
+
+    let numeroAArray = new Array(4);
+
+    if(numero == 6174){
+        llegoAKraprekar = true;
+
+    }else if (numero != 6174){
+        //Bucle que llena a array con los datos del numero transformados a un integer
+        for(let i = 0; i < 4; i++){
+            numeroAArray[i] = +numeroAString.charAt(i);
+        }
     }
+    return numeroAArray;
 }
 
 //
-function ordenarArrays(arrayAOrdenar){
+function ordenarAscendenteYDescendente(arrayAOrdenar){
+
     //Copias para poder ordenar el array dado
-    numeroUsuarioAscendente = arrayAOrdenar.slice();
-    numeroUsuarioDescendente = arrayAOrdenar.slice();
+    arrayAscendente = arrayAOrdenar.slice();
+    arrayDescendente = arrayAOrdenar.slice();
     
     //Ordena de manera ascendente el array
-    numeroUsuarioAscendente.sort((a,b) => a - b);
+    arrayAscendente.sort((a,b) => a - b);
+
     //Ordena de manera descendente el array
-    numeroUsuarioDescendente.sort((a,b) => b - a);
+    arrayDescendente.sort((a,b) => b - a);
 
 }
 
 //
-function kraprekar(){
-    let numeroAscendente = +numeroUsuarioAscendente.join("");
-    let numeroDescendente = +numeroUsuarioDescendente.join("");
-    let resta = numeroAscendente - numeroDescendente;
+function kraprekar(numero){
 
+    let contadorKraprekar = 0;
+    let array;
 
+    array = convertirNumero(numero);
+    ordenarAscendenteYDescendente(array);
+    
+    while(llegoAKraprekar != true){
+
+        let numeroAscendente = +arrayAscendente.join("");
+    
+        let numeroDescendente = +arrayDescendente.join("");
+    
+        let resta = numeroDescendente - numeroAscendente;
+    
+        ordenarAscendenteYDescendente(convertirNumero(resta));
+    
+        contadorKraprekar++;
+    }
+    
+    console.log("Se necesito " + contadorKraprekar + " pasos para llegar a la constante de kraprekar");
 }
 
-convertirNumeroUsuario();
-ordenarArrays(arrayNumeroUsuario);
-
-console.log(numeroUsuarioAscendente);
-console.log(numeroUsuarioDescendente);
-
+kraprekar(numeroUsuario);
